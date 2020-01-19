f17773a9711d7f94fef4d99dd1bd56b274db151a
EvilMorgan93 <morgunof-andrey@yandex.ru>
2020-01-19 11:31:59 +0300

    change ReminderController

--- Controllers/RemindersController.cs
+++ Controllers/RemindersController.cs
@@ -1,36 +1,56 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Reminders_Notifications.Models;
 
 namespace Reminders_Notifications.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RemindersController : ControllerBase
-    {
+    public class RemindersController : ControllerBase {
+
+        private readonly RemindersDataContext dbContext;
+
+        public RemindersController(RemindersDataContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
         // GET: api/Reminders
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
+        public IEnumerable<Reminders> Get() {
+            return dbContext.Reminders.ToList();
         }
 
         // GET: api/Reminders/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
+        [HttpGet("{id}")]
+        public Reminders Get(int id) {
+            var reminder = dbContext.Reminders.FirstOrDefault(x => x.IdReminder == id);
+            return reminder;
         }
 
         // POST: api/Reminders
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Reminders>> Post([FromBody] Reminders reminder)
         {
+            if (ModelState.IsValid) {
+                await dbContext.Reminders.AddAsync(reminder);
+                await dbContext.SaveChangesAsync();
+                return Ok(reminder);
+            }
+            return BadRequest(ModelState);
         }
 
-        // DELETE: api/ApiWithActions/5
+        // DELETE: api/Reminders/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<Reminders>> Delete(int id)
         {
+            var reminder = dbContext.Reminders.FirstOrDefault(x => x.IdReminder == id);
+            if (reminder != null) { 
+                dbContext.Reminders.Remove(reminder);
+                await dbContext.SaveChangesAsync();
+            }
+            return Ok(reminder);
         }
     }
 }
