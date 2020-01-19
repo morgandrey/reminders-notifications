3f3c50ca381d429161e99ab4a6da40ad9ad43583
EvilMorgan93 <morgunof-andrey@yandex.ru>
2020-01-19 10:44:45 +0300

    Добавьте файлы проекта.

---
+++ Controllers/RemindersController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reminders_Notifications.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RemindersController : ControllerBase
+    {
+        // GET: api/Reminders
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Reminders/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Reminders
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
